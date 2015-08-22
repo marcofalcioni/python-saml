2dbcfdd4a780cb237bc25ba0bcaacab3febc8e73
Marco Falcioni <mfalcioni@omicia.com>
2015-08-22 12:23:36 -0700

    working towards a pyramid SSO example

--- demo-pyramid/demopyramid/views.py
+++ demo-pyramid/demopyramid/views.py
@@ -1,14 +1,33 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'demo-pyramid'}
-
+@view_config(route_name='home',
+			 request_method='GET',
+			 renderer='templates/home.pt')
+def home(request):
+    return {
+		'errors': [],
+		'not_auth_warn': False,
+		'success_slo': False,
+		'paint_logout': False,
+	}
 
 @view_config(route_name='home',
 	         match_param='action=login',
 	         request_method='GET',
-	         renderer='templates/login.pt')
+	         renderer='templates/home.pt')
+def login(request):
+	return {
+		'errors': [],
+		'not_auth_warn': False,
+		'success_slo': False,
+		'paint_logout': False,
+	}
+
+
+@view_config(route_name='home',
+	         match_param='action=metadata',
+	         request_method='GET',
+	         renderer='templates/metadata.pt')
 def login(request):
 	return  {}
